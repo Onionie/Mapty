90784187e94b450c6cebed3a23f1f285ddf8f197
Onionie <gutierrez.onionie@gmail.com>
2021-11-14 18:22:18 -0800

    Generate Map using Leaflet

--- script.js
+++ script.js
@@ -16,7 +16,20 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
+
+      const coordinates = [latitude, longitude];
       console.log(position);
+      const map = L.map('map').setView(coordinates, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coordinates)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert("Can't Get Location.");
